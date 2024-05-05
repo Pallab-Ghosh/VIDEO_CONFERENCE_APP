33b77b75f6e1f3c3c2ca6b7d516e5feb4dcc1fb8
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-05 17:28:05 +0530

    Create the layout file and sidebar navbar integration here

---
+++ video_app/app/(root)/(home)/layout.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Sidebar } from './_components/Sidebar'
+import { Navbar } from './_components/Navbar'
+
+
+type HomelayoutProps = {
+    children : React.ReactNode
+}
+
+
+const Homelayout = ({children}:HomelayoutProps) => {
+  return (
+    <main className='relative'>
+
+         <Navbar/>
+          
+       <div className='flex'>
+            <Sidebar/>
+             <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14 bg-slate-400'>
+                 <div className='w-full'>
+                    {children}
+                 </div>
+             </section>
+       </div>
+       
+  
+     
+    </main>
+  )
+}
+
+export default Homelayout
