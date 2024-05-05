8a2faa9358d522c3d6bcc033d3f34a9f5e87a346
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-05 20:05:37 +0530

    Change the css based on which url is active

--- video_app/app/(root)/(home)/_components/SidebarRoutes.tsx
+++ video_app/app/(root)/(home)/_components/SidebarRoutes.tsx
@@ -1,6 +1,9 @@
+'use client'
 import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { cn } from '../../../../lib/utils';
 
 type SideBarProps ={
     label : string
@@ -9,15 +12,21 @@ type SideBarProps ={
 }
 
 export const SidebarRoutes = ({label , src , icon}:SideBarProps) => {
+   
+  const pathName = usePathname();
+  const isActive = pathName === src || pathName.startsWith(src);
   return (
-     <Link href={src} className='font-medium text-lg text-zinc-100'>
-      {label}
-      <Image
-        src={icon}
-        alt='link'
-        width={20}
-        height={20}
-      />
+     <Link href={src}
+      className={cn('font-medium text-lg text-zinc-100 h-12' ,
+        isActive && 'bg-blue-600'
+       )}
+      
+      >
+      <div className='flex gap-x-4 pt-3'>
+         <Image src={icon} alt='link'width={20}  height={20}/>
+         <div>{label}</div>
+      </div>
+     
      </Link>
   )
 }
