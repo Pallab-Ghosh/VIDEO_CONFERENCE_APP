dee59fefe009474be0440dd45b764c3be39ccd4a
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-07 21:41:07 +0530

    Add more Css

--- video_app/app/(root)/(home)/page.tsx
+++ video_app/app/(root)/(home)/page.tsx
@@ -5,8 +5,9 @@ const Homepage = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className='h-[400px] w-full rounded-[20px] mt-10  bg-slate-800 bg-cover flex flex-col relative' >
-             <div className='absolute top-8 left-8 z-10 px-4 py-2 text-white'>
-              <h2 className=' text-white'> Upcoming Meetings at 12.30 PM </h2>
+             <div className='absolute top-8 left-8 z-10 px-4 py-2 text-white flex flex-col'>
+               <h2 className=' text-white bg-slate-700 w-72 h-12 text-center p-3 rounded-md'> Upcoming Meetings at 12.30 PM </h2>
+
              </div>
             <Image  src="/image/hero-background.png"  alt='bg'  height={600}  width={1800}  className=' max-md:hidden flex-1 rounded-[20px]' />
              
