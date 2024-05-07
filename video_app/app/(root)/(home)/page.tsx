54cb80c14798ad3fdfc1ac4f360f7f6b9ca5c8b2
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-07 21:17:13 +0530

    Update the CSS of Home page with Add Image

--- video_app/app/(root)/(home)/page.tsx
+++ video_app/app/(root)/(home)/page.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Homepage = () => {
   return (
-    <section className=' flex size-full flex-col gap-10 text-white'>
-      <h1 className= 'text-2xl font-semibold'>
-        Home
-        </h1>
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <div className='h-[400px] w-full rounded-[20px] mt-10  bg-slate-800 bg-cover flex flex-col relative' >
+             <div className='absolute top-8 left-8 z-10 px-4 py-2 text-white'>
+              <h2 className=' text-white'> Upcoming Meetings at 12.30 PM </h2>
+             </div>
+            <Image  src="/image/hero-background.png"  alt='bg'  height={600}  width={1800}  className=' max-md:hidden flex-1 rounded-[20px]' />
+             
+      </div>
     </section>
   )
 }
