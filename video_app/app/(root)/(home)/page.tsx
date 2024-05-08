239af4da38752c2f9195e5312d56e44535f5ff44
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-09 02:36:56 +0530

    Change the UI that it can be mobile responsive

--- video_app/app/(root)/(home)/page.tsx
+++ video_app/app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import MeetingTypeList from '@/components/MeetingTypeList';
 import Image from 'next/image'
 import React from 'react'
 
@@ -11,9 +12,9 @@ const Homepage = () => {
 
 
   return (
-    <section className='flex size-full flex-col gap-10 text-white'>
+    <section className='flex size-full flex-col gap-12 2xl:gap-28 text-white mt-10 sm:mt-0'>
 
-      <div className='h-[300px] w-full rounded-[20px] mt-10  bg-slate-800 bg-cover flex flex-col relative' >
+      <div className='h-[300px] w-full rounded-[20px] bg-slate-800 bg-cover flex flex-col relative' >
              <div className='absolute top-8 left-8 z-10 px-4 py-2 text-white flex flex-col max-md:py-8 lg:px-11'>
                  <h2 className='rounded-md text-slate-400 bg-slate-700 max-w-[290px] h-12 text-center p-3  font-bold text-base'> Upcoming Meetings at 12.30 PM </h2>
                   
@@ -22,9 +23,10 @@ const Homepage = () => {
                     <p className=' text-xl font-medium'>{date}</p>
                   </div>
              </div> 
-            <Image  src="/image/hero-background.png"  alt='bg'  height={500}  width={1800}  className=' max-md:hidden flex-1 rounded-[20px]' />
+            <Image  src="/image/hero-background.png"  alt='bg'  height={300}  width={1400}  className=' max-md:hidden flex-1 rounded-[20px]' />
       </div>
-
+       
+        <MeetingTypeList/>
     </section>
   )
 }
