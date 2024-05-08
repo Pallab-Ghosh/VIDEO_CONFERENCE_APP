a4b8fb75391631491cea1ca7b834a422273a5bc5
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-09 02:58:27 +0530

    Update layout.tsx

--- video_app/app/(root)/(home)/layout.tsx
+++ video_app/app/(root)/(home)/layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Sidebar } from './_components/Sidebar'
-import { Navbar } from './_components/Navbar'
+import { Navbar } from './_components/nav-bar'
+ 
+
 
 
 type HomelayoutProps = {
@@ -11,20 +13,15 @@ type HomelayoutProps = {
 const Homelayout = ({children}:HomelayoutProps) => {
   return (
     <main className='relative'>
-
-         <Navbar/>
-          
+       <Navbar/>
        <div className='flex'>
             <Sidebar/>
-             <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14 bg-slate-400'>
+             <section className='flex bg-dark-2 min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
                  <div className='w-full'>
                     {children}
                  </div>
              </section>
-       </div>
-       
-  
-     
+       </div>   
     </main>
   )
 }
