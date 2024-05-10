abf2a06a0f61b62c98ab68045cfb3edfe366e71d
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-10 17:23:03 +0530

    Create the call take local state for calls and also for time description link and after call creation push the user to calling window

--- video_app/components/MeetingTypeList.tsx
+++ video_app/components/MeetingTypeList.tsx
@@ -1,5 +1,8 @@
 'use client'
-import HomeCard from '@/app/(root)/(home)/HomeCard'
+import HomeCard from '@/app/(root)/(home)/_components/HomeCard'
+import { MeetingModal } from '@/components/MeetingModal'
+import { useUser } from '@clerk/nextjs'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
 import { PlusSquare } from 'lucide-react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
@@ -12,30 +15,90 @@ const MeetingTypeList = () => {
 
     const [MeetingState ,SetMeetingState]= useState<Meeting_type>();
     const router = useRouter();
+    const user = useUser();
+    const client = useStreamVideoClient();
+    const[values , setvalues] = useState({
+      dateTime : new Date(),
+      description: '',
+      link : ''
+    })
+    const [calldetails , setcalldetails] = useState<Call>()
+
+    const handleClick = async()=>{
+      if(!client || !user)
+        return;
+       
+      try
+       {
+          const id = crypto.randomUUID();
+          const call = client.call('default', id);
+
+          if(!call)throw new Error('Call not successfully created');
+          
+          const startsAt    = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+          const description = values.description || 'Instant Meeting'
+
+          await call.getOrCreate({
+            data: {
+               starts_at : startsAt,
+               custom:{
+                description : description
+               }
+            },
+          });
+          setcalldetails(call);
+
+          if(!values.description)
+            {
+                router.push(`/meeting/${call.id}`)
+            }
+      
+       } 
+
+      catch (error) {
+        
+      }
+
+      
+    }
+
 
   return (
 
     <section className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4'>
-           <HomeCard
-              bgcolor="bg-orange-400" Imagesrc ="/icons/add-meeting.svg"
-              h2text="New Meeting" ptext="Start a Instant Meeting"
-              handleClick = {()=>SetMeetingState('IsInstantMeeting')}
+  
+            <HomeCard
+                bgcolor="bg-orange-400" Imagesrc ="/icons/add-meeting.svg"
+                h2text="New Meeting" ptext="Start a Instant Meeting"
+                handleClick = {()=>SetMeetingState('IsInstantMeeting')}
+              />
+        
+              <HomeCard 
+                bgcolor="bg-blue-500" Imagesrc ="/icons/join-meeting.svg"
+                h2text="Join Meeting" ptext="Via Invitation Link"
+                handleClick = {()=>SetMeetingState('IsJoinMeeting')}
+                />
+        
+             <HomeCard 
+              bgcolor="bg-violet-600" Imagesrc ="/icons/scheduling.svg"
+              h2text="Schedule Meeting" ptext="Plan your Meeting"
+              handleClick = {()=>SetMeetingState('IsScheduleMeeting')}
+              />
+        
+              <HomeCard
+                bgcolor="bg-amber-400" Imagesrc ="/icons/recordings.svg"
+                h2text="View Recordings" ptext="Meeting Recordings"
+                handleClick = {()=>router.push('/recordings')}
+                />
+        
+            <MeetingModal
+             IsOpen = {MeetingState === 'IsInstantMeeting'}
+             OnClose = {()=>SetMeetingState(undefined)}
+             title = "Start an Instant Meeting"
+             buttonText = "Start Meeting"
+             handleClick={handleClick}
             />
-          <HomeCard 
-             bgcolor="bg-blue-500" Imagesrc ="/icons/join-meeting.svg"
-             h2text="Join Meeting" ptext="Via Invitation Link"
-             handleClick = {()=>SetMeetingState('IsJoinMeeting')}
-             />
-          <HomeCard 
-            bgcolor="bg-violet-600" Imagesrc ="/icons/scheduling.svg"
-            h2text="Schedule Meeting" ptext="Plan your Meeting"
-            handleClick = {()=>SetMeetingState('IsScheduleMeeting')}
-            />
-          <HomeCard
-             bgcolor="bg-amber-400" Imagesrc ="/icons/recordings.svg"
-             h2text="View Recordings" ptext="Meeting Recordings"
-             handleClick = {()=>router.push('/recordings')}
-             />
+
     </section>
   )
 }
