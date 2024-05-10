f3fe71d0d322f15e1a5aaa63ebd85675e8c57ac2
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-11 01:25:12 +0530

    Create this components for setup the call and joining into the video room call

---
+++ video_app/app/(root)/meeting/_components/MeetingRoom.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MeetingRoom = () => {
+  return (
+    <div className='text-white'>
+        MeetingRoom
+    </div>
+  )
+}
+
+export default MeetingRoom
---
+++ video_app/app/(root)/meeting/_components/MeetingSetup.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MeetingSetup = () => {
+  return (
+    <div className=' text-white'>
+        MeetingSetup
+    </div>
+  )
+}
+
+export default MeetingSetup
