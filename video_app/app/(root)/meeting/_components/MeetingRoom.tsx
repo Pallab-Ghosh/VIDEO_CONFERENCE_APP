068f4f8346f6a104f9ffb7f4614ced33053df928
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-13 18:25:44 +0530

    Update this component with styles and use the sdk call features .
    Call Features
    1.  <CallControls/>
    2.  <CallLayout/>
    3. <CallParticipantsList/>

--- video_app/app/(root)/meeting/_components/MeetingRoom.tsx
+++ video_app/app/(root)/meeting/_components/MeetingRoom.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import { cn } from '@/lib/utils'
+import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+
+type callLayoutGrid = 'grid' | 'speaker-left' | 'speaker-right'
+
+
 
 const MeetingRoom = () => {
+
+   const [layout , setlayout] = useState<callLayoutGrid>('speaker-left')
+   const[showParticipants , setShowParticipants] = useState(false)
+   const router = useRouter();
+
+   const CallLayout = ()=>{
+    switch (layout) {
+      case 'grid':
+         return <PaginatedGridLayout  />
+         break;
+
+      case 'speaker-left':
+        return <SpeakerLayout participantsBarPosition="left" />
+        break;
+
+      default:
+          case 'speaker-right':
+          return <SpeakerLayout participantsBarPosition="right"  />
+          
+    }
+}  
+
+
   return (
-    <div className='text-white'>
-        MeetingRoom
-    </div>
+    <section className=' h-screen relative w-full overflow-hidden pt-4 text-white'>
+              <div className=' relative flex size-full items-center justify-center'>
+                    <div className='flex size-full max-w-[1000px] items-center'>
+                        <CallLayout/>
+                    </div>
+
+                    <div className={cn('h-[calc(100vh-86px)] hidden ml-2' , showParticipants && 'block' )}>
+                        <CallParticipantsList onClose={()=>setShowParticipants(false)}   />
+                    </div>
+              </div>
+              <div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
+                  <CallControls onLeave={()=>router.push('/')}/>
+              </div>
+    </section>
   )
 }
+
 
 export default MeetingRoom
