2e5fc00c487668d03fb3c5a986e5f1e73cc49666
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-13 18:26:13 +0530

    Remove the previous code for now

--- video_app/app/(root)/meeting/_components/MeetingSetup.tsx
+++ video_app/app/(root)/meeting/_components/MeetingSetup.tsx
@@ -1,7 +1,11 @@
 'use client'
 import { Button } from '@/components/ui/button';
-import { useCall, useCallStateHooks, VideoPreview } from '@stream-io/video-react-sdk'
+import { DeviceSettings, ToggleVideoPreviewButton, ToggleVideoPublishingButton, useCall, useCallStateHooks, VideoPreview } from '@stream-io/video-react-sdk'
+import { Camera, CameraOff, Mic2, MicOff, Video } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
+import VideoOffPreview from './VideoPreview';
+import { cn } from '@/lib/utils';
+import { useMicrophoneMute } from '@/hooks/useMicrophone';
 
 
 
@@ -14,13 +18,18 @@ const MeetingSetup = ({setIsSetupComplete}:MeetingSetupProps) => {
     
      const [isMicCamOn , setisMicCamOn] = useState(false)
      const call = useCall();
+    
      const { useCameraState } = useCallStateHooks();
-     const { camera, isMute } = useCameraState();
+     const { isMute } = useCameraState();
+
+     const IsMicrophoneMute = useMicrophoneMute()
+ 
 
     if(!call) throw new Error('No call is found');
+   
 
     useEffect(()=>{
-      
+         console.log('run')
         if(isMicCamOn)
         {
             call?.camera.disable();
@@ -33,24 +42,24 @@ const MeetingSetup = ({setIsSetupComplete}:MeetingSetupProps) => {
 
         }
       
-    }, [isMicCamOn])
+    }, [isMicCamOn , call?.camera , call?.microphone]) 
 
   return (
-    <div className='h-screen flex w-full flex-col items-center justify-center gap-3 text-white px-2'>
-        <h1 className=' text-2xl font-bold'>Meeting Preview</h1>
-          
-          {
-           !isMute? <VideoPreview/>:(
-             <div className=' h-52 rounded-lg sm:w-96 w-full bg-dark-1 border-2 border-zinc-200 flex justify-center items-center text-white'>
-                No Preview Available
-             </div>
-           )
-          }
-          <label>  { !isMicCamOn ? 'join the Camera and Microphone': 'Off the Camera and Microphone'}</label>
-          <input  checked={isMicCamOn}   type='checkbox'  onChange={(e)=>setisMicCamOn(e.target.checked)} style={{backgroundColor:'greenyellow'}}  />
-          <Button onClick={()=>setIsSetupComplete(true)}  className=' bg-lime-500'>
-             Join the Meeting
-          </Button>
+    <div className='  h-screen flex w-full flex-col items-center justify-center gap-3  text-white '>
+                
+                <h2 className=' text-white sm:text-2xl font-bold'> Meeting Preview</h2>
+                <VideoPreview  />
+                <div className="flex h-16 items-center justify-center gap-3">
+                    <label className="flex items-center justify-center gap-2 font-medium">
+                      <input  type="checkbox"  checked={isMicCamOn} onChange={(e) => setisMicCamOn(e.target.checked)} />
+                        Join with mic and camera off
+                    </label>
+                    <DeviceSettings />
+              </div>
+            <Button className="rounded-md bg-green-500 px-4 py-2.5"  onClick={() => { call.join(); setIsSetupComplete(true); }}>
+              Join meeting
+            </Button>
+                                 
     </div>
   )
 }
