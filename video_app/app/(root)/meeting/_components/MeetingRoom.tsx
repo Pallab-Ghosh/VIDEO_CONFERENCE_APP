e1bbeabecddaa4f792943bfa8cdd59b5e5419754
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-14 13:43:26 +0530

    Update Css and add button for show participants

--- video_app/app/(root)/meeting/_components/MeetingRoom.tsx
+++ video_app/app/(root)/meeting/_components/MeetingRoom.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@/lib/utils'
-import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 import {
@@ -11,8 +11,10 @@ import {
   DropdownMenuTrigger,
   DropdownMenuCheckboxItem,
 } from "@/components/ui/dropdown-menu"
-import { LayoutList } from 'lucide-react'
+import { LayoutList, Users } from 'lucide-react'
 import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
 
 type callLayoutGrid = 'grid' | 'speaker-left' | 'speaker-right' | 'bottom'
 
@@ -29,7 +31,7 @@ const MeetingRoom = () => {
    const CallLayout = ()=>{
     switch (layout) {
       case 'grid':
-         return <PaginatedGridLayout  />
+         return <PaginatedGridLayout  groupSize={10} pageArrowsVisible={true} />
          break;
 
       case 'speaker-left':
@@ -48,25 +50,28 @@ const MeetingRoom = () => {
 
   return (
     <section className=' h-screen relative w-full overflow-hidden pt-4 text-white'>
-              <div className=' relative flex size-full items-center justify-center'>
+              <div className='relative flex size-full items-center justify-center'>
                     <div className='flex size-full max-w-[1000px] items-center'>
                         <CallLayout/>
                     </div>
-
-                    <div className={cn('h-[calc(100vh-86px)] hidden ml-2' , showParticipants && 'block' )}>
-                        <CallParticipantsList onClose={()=>setShowParticipants(false)}   />
+                
+                  <div className={cn('h-[calc(100vh-86px)]  lg:absolute lg:right-0  hidden ml-2' ,{'show-block' :showParticipants })}>
+                        <CallParticipantsList onClose={()=>setShowParticipants(false)}  />
                     </div>
+                    
               </div>
-              <div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
-                  <CallControls onLeave={()=>router.push('/')}/>
+             
 
+              <div className='fixed bottom-0 flex  w-full items-center justify-center gap-5 '>
+             
+                  <CallControls onLeave={()=>router.push('/')}/>
                   <DropdownMenu>
+
                       <div className=' flex items-center'>
                          <DropdownMenuTrigger className=' cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
                             <LayoutList size={20} className=' text-white'/>
                          </DropdownMenuTrigger>
                       </div>
-
 
                     <DropdownMenuContent className=' border-dark-1 bg-dark-1 text-white'>
                       <DropdownMenuLabel>Select Layout</DropdownMenuLabel>
@@ -80,11 +85,17 @@ const MeetingRoom = () => {
                             >
                                {item}
                             </DropdownMenuCheckboxItem>
-                        ))
-}
+                        ))}
                     </DropdownMenuContent>
-                </DropdownMenu>               
+                </DropdownMenu>   
 
+                <CallStatsButton/>
+
+                  <button onClick={()=>setShowParticipants((prev)=>!prev)}>
+                       <div className=' cursor-pointer  rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+                            <Users size={20} className=' text-white' />
+                       </div>
+                  </button>
               </div>
     </section>
   )
