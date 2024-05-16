97a934afbc7ceddbf5becf4db3550ed107a89689
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-16 20:08:10 +0530

    Add buttonIcon and make some props optional

--- video_app/components/MeetingModal.tsx
+++ video_app/components/MeetingModal.tsx
@@ -8,32 +8,39 @@ type MeetingModalProps = {
     IsOpen     : boolean,
     OnClose    : ()=>void,
     title      : string,
-    buttonText : string
-    handleClick: ()=>void,
+    buttonText? : string
+    handleClick?: ()=>void,
     children?  : React.ReactNode , 
     image?     : string
     buttonIcon? : string
-    clasName?   : string
-    scheduleMeeting: boolean
+    className?   : string
+    MeetingCreating?: boolean
 }
 
 
-export function MeetingModal({IsOpen , OnClose , title , buttonText ,scheduleMeeting, handleClick , image , children , buttonIcon , clasName}:MeetingModalProps) {
+export function MeetingModal({IsOpen , OnClose , title , buttonText ,MeetingCreating, handleClick , image , children , buttonIcon , className}:MeetingModalProps) {
 
   return (
     <Dialog open={IsOpen} onOpenChange={OnClose}>
-          <DialogContent className="max-w-[550px] w-full flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+          <DialogContent className="max-w-[550px] w-full flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white" onClick={handleClick}>
               <div className=" flex flex-col gap-6">
                     {
                       image && (
-                        <div>
+                        <div className=" flex justify-center">
                             <Image src={image} alt="image" width={72} height={72}  />
                         </div>
                       )
                     }
-                      <p className={cn(" text-center font-bold text-lg leading-[42px]",clasName)}>{title}</p>
+                      <p className={cn(" text-center font-bold text-lg leading-[42px]",className)}>{title}</p>
                         {children}
-                      <Button  disabled={scheduleMeeting} className=" w-full bg-blue-600 focus-visible:ring-0 focus-visible:ring-offset-0" onClick={handleClick}>
+                      <Button  disabled={MeetingCreating} className=" w-full bg-blue-600 focus-visible:ring-0 focus-visible:ring-offset-0" onClick={handleClick}>
+                      {
+                          buttonIcon && 
+                          (
+                            <Image src={buttonIcon}  alt="button icon"  width={16}  height={16} className=" mr-2" />
+                          )
+                        }
+                        {"  "}
                         {buttonText  || 'Schedule Meeting'}
                       </Button>
               </div>
