d9510189a97846d77dc1b90eb0de74516d4a0e52
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-16 13:26:08 +0530

    Fetch the calling state and render loading if joining

--- video_app/app/(root)/meeting/_components/MeetingRoom.tsx
+++ video_app/app/(root)/meeting/_components/MeetingRoom.tsx
@@ -1,21 +1,16 @@
 import { cn } from '@/lib/utils'
-import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
-import { useRouter, useSearchParams } from 'next/navigation'
-import React, { useState } from 'react'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-  DropdownMenuCheckboxItem,
-} from "@/components/ui/dropdown-menu"
+import { CallControls,TranscriptionSettingsModeEnum, CallingState, CallParticipantsList, CallStatsButton, PaginatedGridLayout, PermissionRequestEvent, SpeakerLayout, StreamVideoEvent, useCall, useCallStateHooks } from '@stream-io/video-react-sdk'
+import { redirect, useRouter, useSearchParams } from 'next/navigation'
+import  { useCallback, useEffect, useState } from 'react'
+import {DropdownMenu,DropdownMenuContent,  DropdownMenuItem,  DropdownMenuLabel,  DropdownMenuSeparator,  DropdownMenuTrigger,  DropdownMenuCheckboxItem,} from "@/components/ui/dropdown-menu"
 import { LayoutList, Users } from 'lucide-react'
 import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
 import EndCallButton from './End-call-button'
+import { Loading } from '@/components/Loading'
+import { MyToggleTranscriptionButton } from './call-transcription'
+
 
 type callLayoutGrid = 'grid' | 'speaker-left' | 'speaker-right' | 'bottom'
 
@@ -27,10 +22,20 @@ const MeetingRoom = () => {
    const [layout , setlayout] = useState<callLayoutGrid>('speaker-left')
    const[showParticipants , setShowParticipants] = useState(false)
    const [showStatus, setShowStatus] = useState<Checked>(false)
+
    const router = useRouter();
    const params = useSearchParams();
+
    const isPersonalRoom = !!params.get('personal')
 
+   const {useCallCallingState} = useCallStateHooks();
+   const callingState = useCallCallingState();
+   const call = useCall()
+  
+     if(callingState !== CallingState.JOINED)
+      return <Loading/> 
+
+      
    
    const CallLayout = ()=>{
     switch (layout) {
@@ -69,7 +74,7 @@ const MeetingRoom = () => {
               <div className='fixed bottom-0 flex  w-full items-center justify-center gap-5 flex-wrap'>
              
                      <CallControls onLeave={()=>router.push('/')}/>
-                  <DropdownMenu>
+                     <DropdownMenu>
 
                       <div className=' flex items-center'>
                          <DropdownMenuTrigger className=' cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
@@ -84,6 +89,7 @@ const MeetingRoom = () => {
                         .map((item, index)=>(
                            
                             <DropdownMenuCheckboxItem
+                              key={index}
                               checked={layout === item}
                               onCheckedChange={()=>setlayout(item as callLayoutGrid)}
                             >
@@ -100,8 +106,9 @@ const MeetingRoom = () => {
                             <Users size={20} className=' text-white' />
                        </div>
                   </button>
-
+                  <MyToggleTranscriptionButton/>
                    {!isPersonalRoom && <EndCallButton/>}
+                   
               </div>
     </section>
   )
