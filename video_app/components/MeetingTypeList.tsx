893317cc12090cbe13e6c77dea4d195e68d4ed64
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-16 20:55:30 +0530

    Show the scheduled time date through toast

--- video_app/components/MeetingTypeList.tsx
+++ video_app/components/MeetingTypeList.tsx
@@ -2,13 +2,18 @@
 import HomeCard from '@/app/(root)/(home)/_components/HomeCard'
 import { MeetingModal } from '@/components/MeetingModal'
 import { useUser } from '@clerk/nextjs'
-import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
+import { Call, useCallStateHooks, useStreamVideoClient } from '@stream-io/video-react-sdk'
 import { PlusSquare } from 'lucide-react'
 import Image from 'next/image'
 import { redirect, useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 import { useToast } from './ui/use-toast'
 import { ToastAction } from './ui/toast'
+
+import ReactDatePicker from 'react-datepicker';
+import { Textarea } from './ui/textarea'
+import MeetingSchedule from './MeetingSchedule'
+
 
 
 
@@ -19,57 +24,58 @@ const MeetingTypeList = () => {
     const { toast } = useToast()
 
     const [MeetingState ,SetMeetingState]= useState<Meeting_type>();
-    const router = useRouter();
-    const user = useUser();
-    const client = useStreamVideoClient();
-    const [scheduleMeeting , setscheduleMeeting] = useState(false)
+    const router = useRouter(); // router
+    const user = useUser();//client user
+    const client = useStreamVideoClient()//stream client
+    const [MeetingCreating , setMeetingCreating] = useState(false) // for button disabled 
+    const [calldetails , setcalldetails] = useState<Call>(); // call details
+   
 
-    const[values , setvalues] = useState({
-      dateTime : new Date(),
-      description: '',
-      link : ''
-    })
+    const[values , setvalues] = useState({  dateTime : new Date(),  description: '',  link : ''})
 
-    const [calldetails , setcalldetails] = useState<Call>();
+   
     
-    const new_date_time = `${values.dateTime.getDay()}/${values.dateTime.getMonth()}/${values.dateTime.getFullYear()}  ${values.dateTime.getHours()}:${values.dateTime.getMinutes()}:${values.dateTime.getSeconds()}`
+    const Meeting_Url=`${process.env.NEXT_PUBLIC_URL}/meeting/${calldetails?.id}`
+    console.log('MEETING_URL',Meeting_Url)
 
     const handleClick = async()=>{
-      if(!client || !user)
-        return;
-       
+
+      
+      if(!client || !user)  return;
+
+      const new_date_time = `${values.dateTime.getDay()}/${values.dateTime.getMonth()}/${values.dateTime.getFullYear()}  ${values.dateTime.getHours()}:${values.dateTime.getMinutes()}:${values.dateTime.getSeconds()}`
+
       try
        {
         
-          setscheduleMeeting(true)
+        setMeetingCreating(true)
           if(!values.dateTime)
             {
-              toast({title : 'Please select a date and time'});
-              return;
+              toast({title : 'Please select a date and time'}); return;
             }
           
-            alert('kk')
+            
           const id = crypto.randomUUID();
-          
           const call = client.call('default', id);
-          
-
           if(!call) throw new Error('Call not successfully created');
           
           const startsAt    = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
           const description = values.description || 'Instant Meeting'
 
-          await call.getOrCreate({
-            data: {  starts_at : startsAt, custom:{ description : description }  }, });
-            setcalldetails(call);
+          await call.getOrCreate({  data: {  starts_at : startsAt, custom:{ description : description }  }, });
+          setcalldetails(call);
+        
 
           if(!values.description)
             {
               router.push(`/meeting/${call.id}`);
               toast({title: " Creating a Meeting Call",description: new_date_time, action: (<ToastAction altText="Goto schedule to undo" onClick={()=>router.push('/')}>Go Back</ToastAction>),})
-              
             }
-      
+            else{
+              toast(
+              {title: " Call scheduled",
+              description: `Meeting Schedule at  ${values.dateTime}`, action: (<ToastAction altText="Goto schedule to undo" onClick={()=>router.push('/')}>Go Back</ToastAction>),})
+            }
        } 
 
       catch (error) {
@@ -78,9 +84,16 @@ const MeetingTypeList = () => {
       }
       
       finally{
-        setscheduleMeeting(false)
-      }
-      
+         setMeetingCreating(false)
+          
+        }
+  }
+
+     
+
+    const handleCreateMeeting = ()=>{
+        navigator.clipboard.writeText(Meeting_Url);
+        toast({title : 'Link copied'})
     }
 
 
@@ -93,6 +106,13 @@ const MeetingTypeList = () => {
                 h2text="New Meeting" ptext="Start a Instant Meeting"
                 handleClick = {()=>SetMeetingState('IsInstantMeeting')}
               />
+
+              <HomeCard 
+              bgcolor="bg-violet-600" Imagesrc ="/icons/scheduling.svg"
+              h2text="Schedule Meeting" ptext="Plan your Meeting"
+              handleClick = {()=>SetMeetingState('IsScheduleMeeting')}
+              />
+        
         
               <HomeCard 
                 bgcolor="bg-blue-500" Imagesrc ="/icons/join-meeting.svg"
@@ -100,26 +120,55 @@ const MeetingTypeList = () => {
                 handleClick = {()=>SetMeetingState('IsJoinMeeting')}
                 />
         
-             <HomeCard 
-              bgcolor="bg-violet-600" Imagesrc ="/icons/scheduling.svg"
-              h2text="Schedule Meeting" ptext="Plan your Meeting"
-              handleClick = {()=>SetMeetingState('IsScheduleMeeting')}
-              />
-        
+           
               <HomeCard
                 bgcolor="bg-amber-400" Imagesrc ="/icons/recordings.svg"
                 h2text="View Recordings" ptext="Meeting Recordings"
                 handleClick = {()=>router.push('/recordings')}
                 />
-        
-            <MeetingModal
-             IsOpen = {MeetingState === 'IsInstantMeeting'}
-             OnClose = {()=>SetMeetingState(undefined)}
-             title = "Start an Instant Meeting"
-             buttonText = "Start Meeting"
-             handleClick={handleClick}
-             scheduleMeeting ={scheduleMeeting}
-            />
+
+                    <MeetingModal
+                    IsOpen = {MeetingState === 'IsInstantMeeting'}
+                    OnClose = {()=>SetMeetingState(undefined)}
+                    title = "Start an Instant Meeting"
+                    buttonText = "Start Meeting"
+                    handleClick={handleClick}
+                    MeetingCreating ={MeetingCreating}
+                    />
+             
+                 {
+                    !calldetails ? 
+                      (
+                        <MeetingModal 
+                         IsOpen = {MeetingState === 'IsScheduleMeeting'}  
+                         OnClose={() => SetMeetingState(undefined)}
+                         title="Create Meeting" 
+                         handleClick={handleClick}
+                         >
+                           <MeetingSchedule values={values} setvalues={setvalues} />
+                      </MeetingModal>
+                      
+                      )
+                        :
+                         
+                        (
+                        <MeetingModal
+                          IsOpen = {MeetingState === 'IsScheduleMeeting'}
+                          OnClose = {()=>SetMeetingState(undefined)}
+                          title = "Meeting Created"
+                          buttonText = "Copy Meeting Link"
+                          handleClick={handleCreateMeeting}
+                          image="/icons/checked.svg"
+                          buttonIcon="/icons/copy.svg"
+                          >
+                          
+                            </MeetingModal>
+                        
+                        )
+
+
+
+               }
 
     </section>
   )
