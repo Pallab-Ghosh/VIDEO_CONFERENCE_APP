b410cdac141df67529cbc5493abf4f244d2a390f
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-18 19:38:59 +0530

    Get the Props from <CallList/>

--- video_app/components/meeting-card.tsx
+++ video_app/components/meeting-card.tsx
@@ -1,6 +1,20 @@
- import React from 'react'
+
+'use client'
  
- const MeetingCard = () => {
+type MeetingCardProps = {
+   
+    icon?              : string
+    title?             : string
+    date ?             : Date
+    isPreviousMeeting : boolean
+    buttonIcon1?      : string
+    hanldeClick ?      : ()=>void
+    link              : string
+    buttonText?        : string 
+}
+
+
+ const MeetingCard = ({ icon , title , date , isPreviousMeeting , buttonIcon1 , buttonText , hanldeClick , link}:MeetingCardProps) => {
    return (
      <div>MeetingCard</div>
    )
