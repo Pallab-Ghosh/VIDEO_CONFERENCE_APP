603af81a88df595a2790d8c5910e51b202698f5b
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-18 18:34:49 +0530

    Create this 2 functions handle_call_type and handle_No_call_message , fetch the suitable data and Integrate <MeetingCard/> component

--- video_app/components/CallList.tsx
+++ video_app/components/CallList.tsx
@@ -1,6 +1,8 @@
+import { useGetCall } from '@/hooks/useGetCall'
 import { CallRecording } from '@stream-io/video-react-sdk'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
+import MeetingCard from './meeting-card'
 
 
 
@@ -12,10 +14,59 @@ const CallList = ({calltype}:CallListProps) => {
    
     const router = useRouter();
     const [recordings, setRecordings] = useState<CallRecording[]>([]);
+    const {endedCalls , upcomingCalls , recordingdata , loading} = useGetCall();
+    
+    const handle_call_type = ()=>{
+       switch(calltype)
+       {
+           case 'ended':
+             return endedCalls ;
+
+           case 'upcoming':
+             return upcomingCalls ;
+
+           case 'recordings':
+              return recordings;
+
+            default :
+              return [];
+       }
+    }
+
+      const handle_No_call_message = ()=>{
+        switch(calltype)
+        {
+            case 'ended':
+              return 'No Previous Calls' ;
+ 
+            case 'upcoming':
+              return 'No Upcoming Calls' ;
+ 
+            case 'recordings':
+               return 'No Recordings';
+ 
+             default :
+               return '';
+        }
+      }
+
+     const calls = handle_call_type();
+     const msgs = handle_No_call_message();
+
+
 
 
   return (
-    <div>CallList</div>
+     <div>
+         {
+             calls && calls.length > 0 ? calls.map(()=>(
+                 <MeetingCard/>
+             )) 
+             :
+               <h1>{msgs}</h1>
+         }
+          
+      </div>
   )
 }
 
