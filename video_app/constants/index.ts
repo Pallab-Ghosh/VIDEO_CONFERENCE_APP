36f60c95b17c5ed19af5d34fdb1f0d524742a3f7
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-19 13:06:36 +0530

    update the right paths of images

--- video_app/constants/index.ts
+++ video_app/constants/index.ts
@@ -4,7 +4,7 @@
 export const avatarImages = [
     '/image/avatar-1.jpeg',
     '/image/avatar-2.jpeg',
-    '/image/avatar-3.jpeg',
-    '/image/avatar-4.jpeg',
-    '/image/avatar-5.jpeg',
+     '/image/avatar-3.png',
+    '/image/avatar-4.png',
+    '/image/avatar-5.png',
 ]
