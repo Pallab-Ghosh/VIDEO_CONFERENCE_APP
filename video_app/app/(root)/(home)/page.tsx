66c1ec026076604e4c731b0541b8a161df2f7934
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-21 02:57:19 +0530

    Fetch the upcoming calls and show the time of first call in home page

--- video_app/app/(root)/(home)/page.tsx
+++ video_app/app/(root)/(home)/page.tsx
@@ -1,6 +1,8 @@
+'use client'
 import MeetingTypeList from '@/components/MeetingTypeList';
 import Image from 'next/image'
 import React from 'react'
+import { useGetCall } from '@/hooks/useGetCall';
 
 const Homepage = () => {
 
@@ -9,20 +11,28 @@ const Homepage = () => {
   const date = (new Intl.DateTimeFormat('en-IN' ,{dateStyle : 'full'} )).format(now)
   const time_mode = time.slice(6,8);//get the time mode separately am/pm
   const new_time = time.slice(0,5) // get the time separately
-
+  const {upcomingCalls} = useGetCall();
+  
+const upcoming_meeting_time = upcomingCalls[0]?.state.startsAt?.toLocaleString().split(',')[1]
+ 
+ 
 
   return (
-    <section className='flex size-full flex-col gap-12 2xl:gap-28 text-white mt-10 sm:mt-0'>
+    <section className='flex size-full flex-col gap-12 2xl:gap-24 text-white mt-10 sm:mt-0'>
 
-      <div className='h-[300px] w-full rounded-[20px] bg-slate-800 bg-cover flex flex-col relative' >
-             <div className='absolute top-8 left-8 z-10 px-4 py-2 text-white flex flex-col max-md:py-8 lg:px-11'>
-                 <h2 className='rounded-md text-slate-400 bg-slate-700 max-w-[290px] h-12 text-center p-3  font-bold text-base'> Upcoming Meetings at 12.30 PM </h2>
+      <div className='h-[300px] w-full rounded-[20px] bg-slate-800 bg-cover flex flex-col  relative' >
+             <div className='absolute top-8 sm:left-8 z-10  text-white flex flex-col  ml-4 max-md:py-8 lg:px-11 max-sm:mr-8'>
                   
+                  <div className=' flex items-center justify-center sm:w-[300px] h-12  w-72  bg-slate-700 rounded-lg'>
+                    <div className=' text-slate-400 sm:h-6 text-center font-bold text-base'> Upcoming Meeting at <span className=' font-bold text-slate-50'>{upcoming_meeting_time}</span></div>
+                  </div>
+
                   <div className=' flex flex-col py-12 md:py-20  gap-3'>
-                    <h1 className=' font-extrabold text-6xl lg:text-7xl'>{new_time} <span className=' text-3xl'>{time_mode}</span></h1>
+                    <h1 className=' font-extrabold text-6xl lg:text-7xl'> <span className=' text-3xl'>{time}</span></h1>
                     <p className=' text-xl font-medium'>{date}</p>
                   </div>
              </div> 
+
             <Image  src="/image/hero-background.png"  alt='bg'  height={300}  width={1400}  className=' max-md:hidden flex-1 rounded-[20px]' />
       </div>
        
