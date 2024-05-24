6fed532331dee9594f4c52df4af3536bd10596b7
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-24 17:45:13 +0530

    Add this Carousel in a dashboard add some images with label and description

---
+++ video_app/app/(root)/(home)/_components/image-slider.tsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+
+import { Card, CardContent } from "@/components/ui/card"
+import {Carousel,CarouselContent,CarouselItem,CarouselNext,CarouselPrevious,} from "@/components/ui/carousel"
+import { image_slider_datas } from "@/constants"
+import Image from "next/image"
+
+export function ImageSlider() {
+  return (
+    <Carousel className="sm:w-[500px] w-full flex justify-center max-w-xs sm:max-w-lg">
+            <CarouselContent>
+                {
+                    image_slider_datas.map((data , index) => (
+                        <CarouselItem key={index}>
+                                <div className="p-1">
+                                    <Card className="bg-dark-2 border-black">
+                                        <CardContent className="flex aspect-square items-center justify-center">
+                                          {
+                                             <div className=" flex flex-col items-center p-4 ">
+                                                 <Image src={data.img_src} height={348} width={348} alt="img"/>
+                                                  <h2 className=" text-white mt-8 mb-4 text-2xl">{data.label}</h2>
+                                                  <p className=" text-white text-center">{data.description}</p>
+                                             </div>
+                                          }
+                                        </CardContent>
+                                    </Card>
+                                </div>
+                        </CarouselItem>
+                    ))
+                }
+            </CarouselContent>
+           <CarouselPrevious />
+           <CarouselNext />
+    </Carousel>
+  )
+}
