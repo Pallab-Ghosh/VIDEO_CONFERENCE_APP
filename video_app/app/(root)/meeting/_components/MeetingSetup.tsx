db606e3331a94726f70b2017709b426186ccdcaa
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-24 19:16:51 +0530

    Change the layout based on camera is on or off and make changes on styles

--- video_app/app/(root)/meeting/_components/MeetingSetup.tsx
+++ video_app/app/(root)/meeting/_components/MeetingSetup.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Button } from '@/components/ui/button';
 import { DeviceSettings, ToggleVideoPreviewButton, ToggleVideoPublishingButton, useCall, useCallStateHooks, VideoPreview } from '@stream-io/video-react-sdk'
-import { Camera, CameraOff, Mic2, MicOff, Video } from 'lucide-react';
+import { Camera, CameraIcon, CameraOff, Mic2, MicOff, Video } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 import VideoOffPreview from './VideoPreview';
 import { cn } from '@/lib/utils';
@@ -28,51 +28,48 @@ const MeetingSetup = ({setIsSetupComplete}:MeetingSetupProps) => {
 
     if(!call) throw new Error('No call is found');
    
-
-   /*  useEffect(()=>{
-    
-        if(isMicCamOn)
-        {
-            call?.camera.disable();
-            call?.microphone.disable();
-        }
-        else
-        {
-            call?.camera.enable();
-            call?.microphone.enable();
-
-        }
-      
-    }, [isMicCamOn , call?.camera , call?.microphone])  */
-
   return (
-    <div className='h-screen flex  max-w-screen flex-col items-center justify-center gap-3  text-white '>
+    <div className='h-screen flex  max-w-screen flex-col items-center justify-center gap-3  text-white relative'>
                 
-                <h2 className=' text-white sm:text-2xl font-bold'> Meeting Preview</h2>
+                { !isMute && <h2 className=' text-white sm:text-2xl font-bold mb-6'>Meeting Preview</h2>}
                 
                   <VideoPreview DisabledVideoPreview={VideoOffPreview} />
-                
+                  {
+                    !isMute && (
+                        <div className=' absolute mt-52'>
+
+                            <div className=' flex gap-5 mb-8'>
+                              {
+                              isMute ? (
+                                    <div className='flex items-center justify-center flex-col  bg-red-600 w-16 rounded-full h-14 '>
+                                      <CameraOff  height={30} width={30} onClick={()=>call?.camera.enable()} /> 
+                                    </div>
+                                ):
+                                (
+                                  <div className='flex items-center justify-center flex-col  bg-red-600 w-16 rounded-full h-14 '>
+                                    <CameraIcon  height={30} width={30} onClick={()=>call?.camera.disable()} /> 
+                                  </div>
+                                )
+                               }
+
+                                <div className={cn('flex items-center justify-center gap-6  w-16 rounded-full h-14',IsMicrophoneMute && 'bg-red-600' , !IsMicrophoneMute && 'bg-transparent'  )}>
+                                    {
+                                        !IsMicrophoneMute ? <Mic2  height={30} width={30} onClick={()=>call?.microphone.disable()} /> : <MicOff  height={30} width={30} onClick={()=>call?.microphone.enable()} />  
+                                    } 
+                                </div>
+                            </div>
+                      </div>
+                    )
+                  }
                
-                   <div className="flex h-16 items-center justify-center gap-3">
-                          <label className="flex items-center justify-center gap-2 font-medium">
-                            <input  type="checkbox"  checked={isMicCamOn} onChange={(e) => setisMicCamOn(e.target.checked)} />
-                              Join with mic and camera off
-                          </label>
+               <div className=' flex justify-center mt-20 gap-1'>
+                 
+                  <Button className="rounded-lg bg-blue-600 px-4 py-2.5"  onClick={() => {  call.join(); setIsSetupComplete(true)}}>
+                      Join meeting
+                  </Button>
 
-
-                        <DeviceSettings />
-                     </div> 
-
-                     
-        
-
-            <Button className="rounded-md bg-green-500 px-4 py-2.5"  onClick={() => { 
-                call.join(); 
-                setIsSetupComplete(true);
-               }}
-               >
-              Join meeting
-            </Button>
+                  <DeviceSettings />
+               </div>
                                  
     </div>
   )
