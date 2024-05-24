09b95da6b45899ec48f01d8d990afcb916d56d08
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-24 17:46:39 +0530

    Add all details like image src description and label as a array of objects and export it

--- video_app/constants/index.ts
+++ video_app/constants/index.ts
@@ -8,3 +8,24 @@ export const avatarImages = [
     '/image/avatar-4.png',
     '/image/avatar-5.png',
 ]
+
+
+export const image_slider_datas = [
+    {
+        img_src : 'https://www.gstatic.com/meet/user_edu_get_a_link_light_90698cd7b4ca04d3005c962a3756c42d.svg',
+        label :   'Get a link you can share',
+        description : 'Click New meeting to get a link you can send to people you want to meet with'
+    },
+
+    {
+        img_src : 'https://www.gstatic.com/meet/user_edu_scheduling_light_b352efa017e4f8f1ffda43e847820322.svg',
+        label :   'Plan ahead',
+        description : 'Click New meeting to schedule meetings in Calendar and send invites to participants'
+    },
+    {
+        img_src : 'https://www.gstatic.com/meet/user_edu_safety_light_e04a2bbb449524ef7e49ea36d5f25b65.svg',
+        label :   'Your meeting is safe',
+        description : 'No one can join a meeting unless Host invited'
+    },
+
+]
