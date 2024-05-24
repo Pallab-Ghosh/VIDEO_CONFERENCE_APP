4da4c8851981b43c6ba26482593fb6eba60f8509
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-24 17:44:21 +0530

    Update the upcoming meeting conditional , if there is meeting show else welcome message and add a card slider

--- video_app/app/(root)/(home)/page.tsx
+++ video_app/app/(root)/(home)/page.tsx
@@ -3,6 +3,7 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 import Image from 'next/image'
 import React from 'react'
 import { useGetCall } from '@/hooks/useGetCall';
+import { ImageSlider } from './_components/image-slider';
 
 const Homepage = () => {
 
@@ -24,19 +25,37 @@ const upcoming_meeting_time = upcomingCalls[0]?.state.startsAt?.toLocaleString()
              <div className='absolute top-8 sm:left-8 z-10  text-white flex flex-col  ml-4 max-md:py-8 lg:px-11 max-sm:mr-8'>
                   
                   <div className=' flex items-center justify-center sm:w-[300px] h-12  w-72  bg-slate-700 rounded-lg'>
-                    <div className=' text-slate-400 sm:h-6 text-center font-bold text-base'> Upcoming Meeting at <span className=' font-bold text-slate-50'>{upcoming_meeting_time}</span></div>
+                     {
+                       upcoming_meeting_time ?(
+                       <div className=' text-slate-400 sm:h-6 text-center font-bold text-base'> 
+                             Upcoming Meeting at <span className=' font-bold text-slate-50'>{upcoming_meeting_time}</span>
+                        </div>
+                       ):
+                       (
+                        <div className=' text-white-400 sm:h-6 text-center font-bold text-base'> 
+                           Hey Folks!! What's next ?
+                        </div>
+                       )
+                     }
+                   
                   </div>
+
+                   
 
                   <div className=' flex flex-col py-12 md:py-20  gap-3'>
                     <h1 className=' font-extrabold text-6xl lg:text-7xl'> <span className=' text-3xl'>{time}</span></h1>
                     <p className=' text-xl font-medium'>{date}</p>
                   </div>
+                  
              </div> 
 
             <Image  src="/image/hero-background.png"  alt='bg'  height={300}  width={1400}  className=' max-md:hidden flex-1 rounded-[20px]' />
       </div>
        
         <MeetingTypeList/>
+          <div className=' flex justify-center w-full bg-dark-2'>
+              <ImageSlider/>
+          </div>
     </section>
   )
 }
