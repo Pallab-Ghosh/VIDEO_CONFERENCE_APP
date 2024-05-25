6b5502778a99207e7da25145b5f467137004cbc8
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-26 02:47:47 +0530

    Here we integrate google api which converts the text into another language call the api and translated the text , for now its a hardcoded language after it will dynamic

--- video_app/app/(root)/meeting/_components/Transcription.tsx
+++ video_app/app/(root)/meeting/_components/Transcription.tsx
@@ -1,16 +1,19 @@
 import { Button } from '@/components/ui/button';
 import { useCall, useCallStateHooks, TranscriptionSettingsModeEnum, } from '@stream-io/video-react-sdk';
-import { useState } from 'react';
-  
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+  console.log('process.env.NEXT_GOOGLE_API',process.env.NEXT_GOOGLE_API_KEY)
 
 
   export const Transcription = () => {
     const call = useCall();
 
     const { useCallSettings, useIsCallTranscribingInProgress } = useCallStateHooks();
-    const [isTranscribing , SetisTranscribing] = useState(false);
+
     const [text , setText]  = useState<string>()
-     
+    const [translation , settranslation]  = useState<string>()
+
     const isActive = false;
     const isSpeechDetecte = false;
     const language = 'en-US';
@@ -19,26 +22,43 @@ import { useState } from 'react';
     function handle_Click() {
 
             const SpeechRecognition  = window.SpeechRecognition || window.webkitSpeechRecognition
-            const recognition = new SpeechRecognition();
+                const recognition = new SpeechRecognition();
 
-            recognition.interimResults = true
-            recognition.onresult = async function(event) {
-                const transcript = event.results[0][0].transcript
-                setText(transcript)
-            }
+                recognition.interimResults = false
+                recognition.maxAlternatives = 1;
+                recognition.continuous = true
+
              
-            recognition.start();
-    }
-  
-    
+                recognition.onresult = async function(event) {
+
+                    const transcript = event.results[event.results.length - 1][0].transcript
+                    setText(transcript)
+
+                    
+                      let res = await axios.post(`https://translation.googleapis.com/language/translate/v2?key=AIzaSyBaagnQuFHgY0fX4ueX1tTKzy4wVrrmNXM`,
+                      { q:transcript , target: "tr" }
+                      );
+
+                      let translation = res.data.data.translations[0].translatedText;
+                      console.log(translation) ;
+                      settranslation(translation)
+                    }
+                   
+                recognition.start();   
+                  
+}
+
+
 
     return (
        <div>
-            <p>{text}</p>
+            
+            <p>{translation}</p>
+            
             <Button onClick={handle_Click}>
               start Transcribing
             </Button>
 
        </div>
-    );
-  };
+    )
+  }
