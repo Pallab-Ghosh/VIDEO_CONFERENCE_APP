39e0c923d51b77a1075c136a1ad60d2381aac99c
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-25 18:38:52 +0530

    Fetch the method of transcription and render the text

--- video_app/app/(root)/meeting/_components/Transcription.tsx
+++ video_app/app/(root)/meeting/_components/Transcription.tsx
@@ -1,28 +1,44 @@
+import { Button } from '@/components/ui/button';
 import { useCall, useCallStateHooks, TranscriptionSettingsModeEnum, } from '@stream-io/video-react-sdk';
 import { useState } from 'react';
   
 
 
   export const Transcription = () => {
-
-
     const call = useCall();
 
     const { useCallSettings, useIsCallTranscribingInProgress } = useCallStateHooks();
     const [isTranscribing , SetisTranscribing] = useState(false);
-   
+    const [text , setText]  = useState<string>()
+     
+    const isActive = false;
+    const isSpeechDetecte = false;
+    const language = 'en-US';
 
-    const handle_Click = ()=>{
-         
-          
-          
+
+    function handle_Click() {
+
+            const SpeechRecognition  = window.SpeechRecognition || window.webkitSpeechRecognition
+            const recognition = new SpeechRecognition();
+
+            recognition.interimResults = true
+            recognition.onresult = async function(event) {
+                const transcript = event.results[0][0].transcript
+                setText(transcript)
+            }
+             
+            recognition.start();
     }
   
     
 
     return (
-      <button onClick={handle_Click}  >
-        {isTranscribing ? 'Stop transcription' : 'Start transcription'}
-      </button>
+       <div>
+            <p>{text}</p>
+            <Button onClick={handle_Click}>
+              start Transcribing
+            </Button>
+
+       </div>
     );
   };
